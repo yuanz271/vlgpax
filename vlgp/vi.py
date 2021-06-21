a0ccac715da8467036936e4941d02c9ebb074cab
yuan <yuanz271@gmail.com>
2021-06-21 13:43:43 -0400

    skip if nan or inf steps

--- vlgp/vi.py
+++ vlgp/vi.py
@@ -21,22 +21,21 @@
 # K: Array(M, T, T), prior kernel matrices, reference to some irredundant storage
 # L: Array(M, T, S), K ~= LL'
 ######################
-from typing import Union, Sequence
+import time
+from typing import Union, Sequence, Tuple, Any
 
 import click
-import jax
 from jax import numpy as jnp
 from sklearn.decomposition import FactorAnalysis
 
-from .data import Experiment, Params
+from .data import Session, Params
 from .gp import kernel
 from .util import diag_embed, stable_solve as solve, capped_exp
-
 
 __all__ = ['Inference']
 
 
-def trial_update(y, Cx, Cz, x, z, v, w, K, logdet, eps):
+def trial_update(y, Cx, Cz, x, z, v, w, K, logdet, eps) -> Tuple[float, Any]:
     T = y.shape[0]
     u = v @ (Cz ** 2)
     lnr = x @ Cx + z @ Cz
@@ -74,15 +73,15 @@ def trial_update(y, Cx, Cz, x, z, v, w, K, logdet, eps):
     return loss, step
 
 
-def estep(infer, *, max_iter: int = 20, stepsize=0.9, eps: float = 1e-8):
+def estep(infer, *, max_iter: int = 20, stepsize=.5, eps: float = 1e-8) -> jnp.ndarray:
     params = infer.params
     zdim = params.n_factors
     C = params.C  # (zdim + xdim, ydim)
     Cz, Cx = jnp.vsplit(C, [zdim])  # (n_factors + n_regressors, n_channels)
 
-    total_loss = 0.
+    total_loss: jnp.ndarray = jnp.array(0.)
     total_T = 0
-    for trial in infer.experiment:  # parallelizable
+    for trial in infer.session:  # parallelizable
         x = trial.x  # regressors
         z = trial.z
         y = trial.y
@@ -98,10 +97,11 @@ def estep(infer, *, max_iter: int = 20, stepsize=0.9, eps: float = 1e-8):
 
             if jnp.isclose(loss, new_loss):
                 break
-
             loss = new_loss
-            newton_step *= stepsize
-            z -= newton_step
+
+            if jnp.any(jnp.isnan(newton_step)) or jnp.any(jnp.isinf(newton_step)):
+                break
+            z -= stepsize * newton_step
 
         trial.z = z
         trial.w = w
@@ -111,16 +111,18 @@ def estep(infer, *, max_iter: int = 20, stepsize=0.9, eps: float = 1e-8):
     return total_loss / total_T
 
 
-def mstep(infer, *, max_iter: int = 20):
+def mstep(infer, *, max_iter: int = 20, stepsize=.5):
     params = infer.params
     zdim = params.n_factors
     C = params.C  # (zdim + xdim, ydim)
+
     # concat trials
-    y = jnp.row_stack([trial.y for trial in infer.experiment])
-    x = jnp.row_stack([trial.x for trial in infer.experiment])
-    z = jnp.row_stack([trial.z for trial in infer.experiment])
-    v = jnp.row_stack([trial.v for trial in infer.experiment])
+    y = infer.session.y
+    x = infer.session.x
+    z = infer.session.z
+    v = infer.session.v
     M = jnp.column_stack((z, x))
+
     loss = jnp.inf
     for i in range(max_iter):
         Cz = C[:zdim, :]
@@ -140,31 +142,31 @@ def mstep(infer, *, max_iter: int = 20):
         g = (r - y).T @ M  # (ydim, zdim + xdim)
         H = jnp.expand_dims(M.T, 0) @ R @ jnp.expand_dims(M, 0)  # (ydim, zdim + xdim, zdim + xdim)
         step = jnp.squeeze(solve(H, jnp.expand_dims(g, -1)), -1).T  # (ydim, ?, 1)
-        C -= step
+        C -= stepsize * step
 
     params.C = C
     return loss
 
 
 class Inference:
-    def __init__(self, experiment: Experiment, n_factors: int, *,
+    def __init__(self, session: Session, n_factors: int, *,
                  scale: Union[float, Sequence[float]] = 1.,
                  lengthscale: Union[float, Sequence[float]] = 1.):
-        self.experiment = experiment
+        self.session = session
         self.params = Params(n_factors, scale, lengthscale)
         self.init()
 
     def init(self):
-        assert self.experiment.trials
-        trial = self.experiment.trials[0]
+        assert self.session.trials
+        trial = self.session.trials[0]
         n_channels = trial.y.shape[-1]
         n_regressors = trial.x.shape[-1]
         self.params.C = jnp.zeros((self.params.n_factors + n_regressors, n_channels))
-        unique_Ts = jnp.unique([trial.y.shape[0] for trial in self.experiment])
+        unique_Ts = jnp.unique([trial.y.shape[0] for trial in self.session])
         ks = [kernel.RBF(scale, lengthscale, jitter=1e-3) for scale, lengthscale in
               zip(self.params.scale, self.params.lengthscale)]
         self.params.K = {
-            T: jnp.stack([k(jnp.arange(T * self.experiment.binsize, step=self.experiment.binsize)) for k in ks]) for T
+            T: jnp.stack([k(jnp.arange(T * self.session.binsize, step=self.session.binsize)) for k in ks]) for T
             in unique_Ts}
         self.params.L = {
             T: jnp.linalg.cholesky(K)
@@ -175,15 +177,15 @@ class Inference:
             for T, L in self.params.L.items()
         }
 
-        for trial in self.experiment:
+        for trial in self.session:
             trial.K = self.params.K[trial.y.shape[0]]
             trial.L = self.params.L[trial.y.shape[0]]
             trial.logdet = self.params.logdet[trial.y.shape[0]]
 
         fa = FactorAnalysis(n_components=self.params.n_factors)
-        y = jnp.row_stack([trial.y for trial in self.experiment])
+        y = jnp.row_stack([trial.y for trial in self.session])
         fa = fa.fit(y)
-        for trial in self.experiment:
+        for trial in self.session:
             trial.z = jnp.asarray(fa.transform(trial.y))
             assert trial.z.shape[0] == (trial.y.shape[0])
             trial.v = jnp.tile(self.params.scale, (trial.z.shape[0], 1))
@@ -192,10 +194,25 @@ class Inference:
     def fit(self, *, max_iter: int = 20, tol: float = 1e-7):
         loss = jnp.inf
         for i in range(max_iter):
+            tick = time.perf_counter()
             m_loss = mstep(self)
-            e_loss = estep(self)
-            click.echo(f'Iteration {i + 1}, M step: {m_loss.item():.2f}, E step: {e_loss.item():.2f}')
+            tock = time.perf_counter()
+            m_elapsed = tock - tick
 
+            tick = time.perf_counter()
+            e_loss = estep(self)
+            tock = time.perf_counter()
+            e_elapsed = tock - tick
+
+            click.echo(
+                f'Iteration {i + 1},\tLoss = {e_loss.item():.2f},\t'
+                f'M step: {m_elapsed:.2f}s,\t'
+                f'E step: {e_elapsed:.2f}s'
+            )
+
+            if jnp.isnan(e_loss):
+                click.echo('Stopped at NaN loss.')
+                break
             if jnp.isclose(loss, e_loss):
                 click.echo('Stopped at unchanged loss.')
                 break
