78bf9905dc118c77c0d74cf209faf7439eaa9d69
yuan <yuanz271@gmail.com>
2021-06-21 10:12:14 -0400

    extract newton step

--- vlgp/vi.py
+++ vlgp/vi.py
@@ -24,12 +24,55 @@
 from typing import Union, Sequence
 
 import click
+import jax
 from jax import numpy as jnp
 from sklearn.decomposition import FactorAnalysis
 
 from .data import Experiment, Params
 from .gp import kernel
 from .util import diag_embed, stable_solve as solve, capped_exp
+
+
+__all__ = ['Inference']
+
+
+@jax.jit
+def trial_update(y, Cx, Cz, x, z, v, w, K, logdet, eps):
+    T = y.shape[0]
+    u = v @ (Cz ** 2)
+    lnr = x @ Cx + z @ Cz
+    r = capped_exp(lnr + 0.5 * u)  # [x, z] C
+    # assert not jnp.any(jnp.isnan(r))
+    w = r @ (Cz.T ** 2)
+    # assert not jnp.any(jnp.isnan(w))
+    z3d = jnp.expand_dims(z.T, -1)
+    K_div_z = solve(K, z3d)
+    # assert not jnp.any(jnp.isnan(K_div_z))
+
+    invw = 1 / (w + eps)
+    invW = diag_embed(invw.T)  # (zdim, T, T)
+    V = K - K @ solve(invW + K, K)  # (zdim, T, T)
+    # assert not jnp.any(jnp.isnan(V))
+    ll = jnp.sum(r - y * lnr)  # likelihood
+    lp = 0.5 * jnp.sum(logdet +
+                       jnp.squeeze(jnp.transpose(z3d, (0, 2, 1)) @ K_div_z, -1) +
+                       jnp.trace(jnp.linalg.solve(K, V), axis1=-2, axis2=-1))
+    lq = -0.5 * jnp.sum(jnp.log(jnp.linalg.cholesky(V).diagonal(axis1=-2, axis2=-1)).sum(-1) * 2)
+    # l2 = jnp.sum((jnp.sum(z ** 2, 0) - T) ** 2)
+    l2 = 0.
+    loss = (ll + lp + lq + l2) / T
+
+    # Newton step
+    g = K_div_z - jnp.expand_dims(Cz @ (y - r).T, -1)  # (zdim, T, T) (zdim, T, 1)
+    # z3d2 = jnp.sum(z3d ** 2, axis=1, keepdims=True)
+    # assert not jnp.any(jnp.isnan(z3d2))
+    # g += (2 * z3d2 - T) * z3d
+    # Hl2 = 6 * z3d2 - T
+    # assert not jnp.any(jnp.isnan(g))
+    # invH = V - V @ solve(1 / (Hl2 + eps) + V, V)
+    step = jnp.squeeze(V @ g, -1).T  # V = inv(-Hessian)
+    # assert not jnp.any(jnp.isnan(step))
+    return loss, step
 
 
 def estep(infer, *, max_iter: int = 20, stepsize=0.9, eps: float = 1e-8):
@@ -52,43 +95,16 @@ def estep(infer, *, max_iter: int = 20, stepsize=0.9, eps: float = 1e-8):
         T = y.shape[0]
         loss = jnp.inf
         for i in range(max_iter):
-            u = v @ (Cz ** 2)
-            lnr = x @ Cx + z @ Cz
-            r = capped_exp(lnr + 0.5 * u)  # [x, z] C
-            assert not jnp.any(jnp.isnan(r))
-            w = r @ (Cz.T ** 2)
-            assert not jnp.any(jnp.isnan(w))
-            z3d = jnp.expand_dims(z.T, -1)
-            K_div_z = solve(K, z3d)
-            assert not jnp.any(jnp.isnan(K_div_z))
-
-            invw = 1 / (w + eps)
-            invW = diag_embed(invw.T)  # (zdim, T, T)
-            V = K - K @ solve(invW + K, K)  # (zdim, T, T)
-            assert not jnp.any(jnp.isnan(V))
-            ll = jnp.sum(r - y * lnr)  # likelihood
-            lp = 0.5 * jnp.sum(logdet +
-                               jnp.squeeze(jnp.transpose(z3d, (0, 2, 1)) @ K_div_z, -1) +
-                               jnp.trace(jnp.linalg.solve(K, V), axis1=-2, axis2=-1))
-            lq = -0.5 * jnp.sum(jnp.log(jnp.linalg.cholesky(V).diagonal(axis1=-2, axis2=-1)).sum(-1) * 2)
-            l2 = jnp.sum((jnp.sum(z ** 2, 0) - T) ** 2)
-            new_loss = (ll + lp + lq + l2) / T
+            new_loss, newton_step = trial_update(y, Cx, Cz, x, z, v, w, K, logdet, eps)
 
             if jnp.isclose(loss, new_loss):
                 break
-            loss = new_loss
 
-            # Newton update
-            g = K_div_z - jnp.expand_dims(Cz @ (y - r).T, -1)  # (zdim, T, T) (zdim, T, 1)
-            # z3d2 = jnp.sum(z3d ** 2, axis=1, keepdims=True)
-            # assert not jnp.any(jnp.isnan(z3d2))
-            # g += (2 * z3d2 - T) * z3d
-            # Hl2 = 6 * z3d2 - T
-            # assert not jnp.any(jnp.isnan(g))
-            # invH = V - V @ solve(1 / (Hl2 + eps) + V, V)
-            step = stepsize * jnp.squeeze(V @ g, -1).T  # V = inv(-Hessian)
-            assert not jnp.any(jnp.isnan(step))
-            z -= step
+            loss = new_loss
+            newton_step *= stepsize
+            if jnp.any(jnp.isnan(newton_step)) or jnp.any(jnp.isinf(newton_step)):
+                continue
+            z -= newton_step
 
         trial.z = z
         trial.w = w
