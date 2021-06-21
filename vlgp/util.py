c93ff97d234b38cdbde3863ee71d06856bbabbc8
yuan <yuanz271@gmail.com>
2021-06-21 10:08:08 -0400

    remove flag for jit no numerical issue with capped exp

--- vlgp/util.py
+++ vlgp/util.py
@@ -1,3 +1,5 @@
+import warnings
+
 import jax
 from jax import numpy as jnp
 
@@ -7,16 +9,17 @@ diag_embed = jax.jit(jax.vmap(jnp.diag))
 batch_lstsq = jax.jit(jax.vmap(lambda a, b: jnp.linalg.lstsq(a, b)[0]))
 
 
+@jax.jit
 def stable_solve(a, b):
-    valid = True
+    # valid = True
     try:
         x = jnp.linalg.solve(a, b)
-        if jnp.any(jnp.isnan(x)) or jnp.any(jnp.isinf(x)):
-            valid = False
+        # if jnp.any(jnp.isnan(x)) or jnp.any(jnp.isinf(x)):
+        #     valid = False
     except RuntimeError:
-        valid = False
-
-    if not valid:
+        # valid = False
+    # if not valid:
+        warnings.warn('Fall back to least squares')
         x = batch_lstsq(a, b)
 
     return x
