f529e09c4a43f256204ca2d52eefc734307d2373
yuan <yuanz271@gmail.com>
2021-06-23 17:23:42 -0400

    try to improve numerical stability

--- vlgp/vi.py
+++ vlgp/vi.py
@@ -26,16 +26,15 @@ from collections import Iterable
 from typing import Union, Sequence, Tuple, Any, Callable
 
 import typer
-from jax import numpy as jnp
+from jax import lax, numpy as jnp
 from sklearn.decomposition import FactorAnalysis
 
 from .data import Session, Params, Trial
-from .util import diag_embed, stable_solve as solve, capped_exp
+from .util import diag_embed, stable_solve as solve, capped_exp, cholesky_solve
 
 __all__ = ['Inference']
 
 
-def trial_update(y, Cx, Cz, x, z, v, w, K, logdet, eps) -> Tuple[float, Any]:
 def reconstruct_cov(K, w, eps=1e-7):
     invw = 1. / (w + eps)
     assert jnp.all(invw > 0.)
@@ -48,19 +47,20 @@ def reconstruct_cov(K, w, eps=1e-7):
     Vd = diag_embed(jnp.clip(V.diagonal(axis1=-2, axis2=-1), a_max=0.) + eps)
     V = V + Vd  # make sure V is PD
     return V
+
+
+def single_trial_step(y, Cx, Cz, x, z, v, K, L, logdet, eps):
     u = v @ (Cz ** 2)
     lnr = x @ Cx + z @ Cz
     r = capped_exp(lnr + 0.5 * u)  # [x, z] C
     # assert not jnp.any(jnp.isnan(r))
     w = r @ (Cz.T ** 2)
+    assert jnp.all(w > 0.)
     # assert not jnp.any(jnp.isnan(w))
     z3d = jnp.expand_dims(z.T, -1)
-    K_div_z = solve(K, z3d)
-    # assert not jnp.any(jnp.isnan(K_div_z))
+    z_div_K = cholesky_solve(L, z3d)
+    # assert not jnp.any(jnp.isnan(z_div_K))
 
-    invw = 1 / (w + eps)
-    invW = diag_embed(invw.T)  # (zdim, T, T)
-    V = K - K @ solve(invW + K, K)  # (zdim, T, T)
     # invw = 1 / (w + eps)
     # assert jnp.all(invw > 0.)
     # invW = diag_embed(invw.T)  # (zdim, T, T)
@@ -72,28 +72,25 @@ def reconstruct_cov(K, w, eps=1e-7):
     # Vd = diag_embed(jnp.clip(V.diagonal(axis1=-2, axis2=-1), a_max=0.) + eps)
     # V = V + Vd  # make sure V is PD
     V = reconstruct_cov(K, w, eps)
+    v = V.diagonal(axis1=-2, axis2=-1).T
+    assert jnp.all(V.diagonal(axis1=-2, axis2=-1) > 0.)
     # assert not jnp.any(jnp.isnan(V))
     ll = jnp.sum(r - y * lnr)  # likelihood
     lp = 0.5 * jnp.sum(logdet +
-                       jnp.squeeze(jnp.transpose(z3d, (0, 2, 1)) @ K_div_z, -1) +
-                       jnp.trace(jnp.linalg.solve(K, V), axis1=-2, axis2=-1))
+                       jnp.squeeze(jnp.transpose(z3d, (0, 2, 1)) @ z_div_K, -1) +
+                       jnp.trace(cholesky_solve(L, V), axis1=-2, axis2=-1))
     lq = -0.5 * jnp.sum(jnp.log(jnp.linalg.cholesky(V).diagonal(axis1=-2, axis2=-1)).sum(-1) * 2)
     loss = ll + lp + lq
 
     # Newton step
-    g = K_div_z - jnp.expand_dims(Cz @ (y - r).T, -1)  # (zdim, T, T) (zdim, T, 1)
-    # z3d2 = jnp.sum(z3d ** 2, axis=1, keepdims=True)
-    # assert not jnp.any(jnp.isnan(z3d2))
-    # g += (2 * z3d2 - T) * z3d
-    # Hl2 = 6 * z3d2 - T
-    # assert not jnp.any(jnp.isnan(g))
-    # invH = V - V @ solve(1 / (Hl2 + eps) + V, V)
-    step = jnp.squeeze(V @ g, -1).T  # V = inv(-Hessian)
+    g = z_div_K + jnp.expand_dims(Cz @ (r - y).T, -1)  # (zdim, T, T) (zdim, T, 1)
+    invH = V
+    step = jnp.squeeze(invH @ g, -1).T  # V = inv(-Hessian)
     # assert not jnp.any(jnp.isnan(step))
-    return loss, step
+    return loss, step, v, w
 
 
-def estep(session, params, *, max_iter: int = 20, stepsize=1., eps: float = 1e-8) -> jnp.ndarray:
+def estep(session, params, *, max_iter: int = 20, stepsize=1., eps: float = 1e-7, clip=1.) -> jnp.ndarray:
     zdim = params.n_factors
     C = params.C  # (zdim + xdim, ydim)
     Cz, Cx = jnp.vsplit(C, [zdim])  # (n_factors + n_regressors, n_channels)
@@ -107,22 +104,24 @@ def estep(session, params, *, max_iter: int = 20, stepsize=1., eps: float = 1e-8
         v = trial.v
         w = trial.w
         K = trial.K
+        L = trial.L
         logdet = trial.logdet
 
         T = y.shape[0]
         loss = jnp.inf
         for i in range(max_iter):
-            new_loss, newton_step = trial_update(y, Cx, Cz, x, z, v, w, K, logdet, eps)
             new_loss, newton_step, v, w = single_trial_step(y, Cx, Cz, x, z, v, K, L, logdet, eps)
 
             if jnp.isclose(loss, new_loss):
                 break
             loss = new_loss
 
-            newton_step = jnp.clip(newton_step, a_min=-stepsize, a_max=stepsize)
+            if jnp.any(jnp.abs(newton_step) > 5.):
+                typer.echo(f'E: large update detected')
+            newton_step = jnp.clip(newton_step, a_min=-clip, a_max=clip)
             if jnp.any(jnp.isnan(newton_step)) or jnp.any(jnp.isinf(newton_step)):
                 break
-            z -= stepsize * newton_step
+            z = z - stepsize * newton_step
 
         trial.z = z
         trial.v = v
@@ -133,7 +132,7 @@ def estep(session, params, *, max_iter: int = 20, stepsize=1., eps: float = 1e-8
     return total_loss / total_T
 
 
-def mstep(session, params, *, max_iter: int = 20, stepsize=1.):
+def mstep(session, params, *, max_iter: int = 20, stepsize=1., clip=1.):
     zdim = params.n_factors
     C = params.C  # (zdim + xdim, ydim)
 
@@ -162,8 +161,12 @@ def mstep(session, params, *, max_iter: int = 20, stepsize=1.):
         # Newton update
         g = (r - y).T @ M  # (ydim, zdim + xdim)
         H = jnp.expand_dims(M.T, 0) @ R @ jnp.expand_dims(M, 0)  # (ydim, zdim + xdim, zdim + xdim)
-        step = jnp.squeeze(solve(H, jnp.expand_dims(g, -1)), -1).T  # (ydim, ?, 1)
-        C -= stepsize * step
+        assert jnp.all(H.diagonal(axis1=-2, axis2=-1) > 0.)
+        newton_step = jnp.squeeze(solve(H, jnp.expand_dims(g, -1)), -1).T  # (ydim, ?, 1)
+        if jnp.any(jnp.abs(newton_step) > 5.):
+            typer.echo(f'M: large update detected')
+        newton_step = jnp.clip(newton_step, a_min=-clip, a_max=clip)
+        C = C - stepsize * newton_step
 
     params.C = C
     return loss
@@ -244,38 +247,11 @@ class Inference:
         typer.echo('Initializing')
         preprocess(self.session, self.params, initialize=fa.transform)
         preprocess(self.em_session, self.params, initialize=fa.transform)
+        typer.secho('Initialized', fg=typer.colors.GREEN, bold=True)
 
-    def fit(self, *, max_iter: int = 20, tol: float = 1e-7):
-        typer.echo('EM starting')
+    def fit(self, *, max_iter: int = 20):
         loss = jnp.inf
-        for i in range(max_iter):
-            tick = time.perf_counter()
-            mstep(self.em_session, self.params)
-            tock = time.perf_counter()
-            m_elapsed = tock - tick
 
-            tick = time.perf_counter()
-            new_loss = estep(self.em_session, self.params)
-            tock = time.perf_counter()
-            e_elapsed = tock - tick
-
-            typer.echo(
-                f'EM Iteration {i + 1},\tLoss = {new_loss.item():.2f},\t'
-                f'M step: {m_elapsed:.2f}s,\t'
-                f'E step: {e_elapsed:.2f}s'
-            )
-
-            if jnp.isnan(new_loss):
-                typer.echo(typer.style('EM stopped at NaN loss.', fg=typer.colors.WHITE, bg=typer.colors.RED))
-                break
-            if jnp.isclose(loss, new_loss):
-                typer.echo('EM stopped at unchanged loss.')
-                break
-            # if new_loss > loss:
-            #     typer.echo('EM stopped at increased loss.')
-            #     break
-
-            loss = new_loss
         try:
             for i in range(max_iter):
                 tick = time.perf_counter()
@@ -310,6 +286,6 @@ class Inference:
 
         typer.echo('Inferring')
         estep(self.session, self.params)
-        typer.echo(typer.style('Finished', fg=typer.colors.GREEN, bold=True))
+        typer.secho('Finished', fg=typer.colors.GREEN, bold=True)
 
         return self
